13c77be255f4ecf08768755008569569fe8b7dca
MosheHadadG <Moshehadad500@gmail.com>
2022-06-28 22:50:43 +0300

    Fixed Imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import Homepage from "./components/Homepage/Homepage";
 import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Language from "./components/lang/Language";
-import EventPage from "./Screens/Event_Page/Eventpage";
-import HomePage from "./Screens/Home_page/Homepage";
+import EventPage from "./Screens/Event_Page/EventPage";
+import HomePage from "./Screens/Home_page/HomePage";
 
 function App() {
   return (
@@ -15,7 +14,7 @@ function App() {
         <Route path='/event' exact element={<EventPage />} />
         <Route path='/' exact element={<HomePage />} />
         {/* 404 page */}
-        <Route path='/*' element={<Homepage />} />
+        <Route path='/*' element={<HomePage />} />
       </Routes>
     </div>
   );
