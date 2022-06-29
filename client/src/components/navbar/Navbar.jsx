6be96f4ab3839fd53bcf975733acba1d144da406
MosheHadadG <Moshehadad500@gmail.com>
2022-06-29 14:04:44 +0300

    Adding Style Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import DropdownExampleSearchDropdown from "../Language/LanguageSelect";
 import "./navbar.css";
 export default function Navbar() {
+
   return (
     <div className='navbar'>
-      <div className='navbarlogo'>tripper.</div>
-      <Link to='/'>Homepage</Link>
-      <Link to='/event'>Events</Link>
+      <div className="ui inverted segment">
+        <div className="ui inverted secondary pointing menu">
+          <div className="navbar-right-side">
+            <NavLink to='/'><div className='navbarlogo'>tripper.</div></NavLink>
+            <NavLink className="item" to='/'>Home</NavLink>
+            <NavLink className="item" to='/login'>Log In</NavLink>
+            <NavLink className="item" to='/signup'>Sign Up</NavLink>
+            <NavLink className="item" to='/events'>Events</NavLink>
+          </div>
+          <div className="navber-left-side">
+            <DropdownExampleSearchDropdown />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
