f1b96fffecd3e45a1dde2b04c532c2700df80474
MichaelVez <michaelx979@gmail.com>
2022-06-29 14:39:11 +0300

    app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const usersRouter = require("./routers/users.router");
 const countryRouter = require("./routers/countries.router");
-// import { someRouter } from "./routes/router.js";
 const app = express();
-// import path from "path";
 const path = require("path");
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use("/users", usersRouter);
@@ -19,4 +14,3 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
 module.exports = app;
-// export { app };
