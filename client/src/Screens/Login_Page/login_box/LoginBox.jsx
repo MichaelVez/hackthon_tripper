6982c1f3a3e3d4fccdf9d21c80af3e38986d744a
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-29 23:25:09 +0300

    translate what new

--- client/src/Screens/Login_Page/login_box/LoginBox.jsx
+++ client/src/Screens/Login_Page/login_box/LoginBox.jsx
@@ -1,79 +1,91 @@
-import React, { useContext, useState } from 'react'
-import './LoginBox.css'
+import React, { useContext, useState } from "react";
+import "./LoginBox.css";
 // import { loginUser } from '../../API/ServerAPI/server.users';
-import { appContext } from '../../../context/appContext';
-import { useNavigate } from 'react-router-dom';
-import Spinner from '../../../components/Spinner/Spinner'
-import { loginUser } from '../../../api/users.api';
+import { appContext } from "../../../context/appContext";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../../components/Spinner/Spinner";
+import { loginUser } from "../../../api/users.api";
+import { useTranslation } from "react-i18next";
 
 const initialState = {
-  email: '',
-  password: '',
-}
-
+  email: "",
+  password: "",
+};
 
 function LoginBox({ loginMode, setLoginMode }) {
   const [formLogin, setFormLogin] = useState(initialState);
-  const [wrongMsg, setWrongMsg] = useState(false)
-  const [spinner, setSpinner] = useState(false)
-
+  const [wrongMsg, setWrongMsg] = useState(false);
+  const [spinner, setSpinner] = useState(false);
 
   let navigate = useNavigate();
   // Global State
   const { setToken, setUserSignIn } = useContext(appContext);
 
   const handleChange = ({ target: { name: nameInput, value } }) => {
-    setFormLogin({ ...formLogin, [nameInput]: value })
-
-  }
-
+    setFormLogin({ ...formLogin, [nameInput]: value });
+  };
 
   const handleClick = async (event) => {
-    event.preventDefault()
-    if(!formLogin.email || !formLogin.password) return setWrongMsg("Please enter all fields")
-    setSpinner(true)
+    event.preventDefault();
+    if (!formLogin.email || !formLogin.password)
+      return setWrongMsg("Please enter all fields");
+    setSpinner(true);
     const userLogin = await loginUser(formLogin);
-    setSpinner(false)
+    setSpinner(false);
     console.log(userLogin);
     if (userLogin.error) return setWrongMsg(userLogin.error);
-
 
     const userToken = userLogin.token;
     setToken(userToken);
     setUserSignIn(userLogin.user);
-    localStorage.setItem('token', userToken);
-    localStorage.setItem('user', JSON.stringify(userLogin.user));
-    navigate('/')
-
-  }
-
+    localStorage.setItem("token", userToken);
+    localStorage.setItem("user", JSON.stringify(userLogin.user));
+    navigate("/");
+  };
+  const { t } = useTranslation();
   return (
-    <div className='login-container'>
-
-      {spinner ? <Spinner /> :
+    <div className="login-container">
+      {spinner ? (
+        <Spinner />
+      ) : (
         <>
-          <form className='login-form' action="">
-            <div className='login-inputs'>
+          <form className="login-form" action="">
+            <div className="login-inputs">
+              <input
+                onChange={handleChange}
+                placeholder={t("form.email")}
+                name="email"
+                value={formLogin.email}
+                type="email"
+                required
+              />
 
-              <input onChange={handleChange} placeholder="Email"
-                name='email' value={formLogin.email} type="email" required />
-
-              <input onChange={handleChange} placeholder="Password"
-                name='password' autoComplete="true" password={formLogin.password} type="password" required />
+              <input
+                onChange={handleChange}
+                placeholder={t("form.password")}
+                name="password"
+                autoComplete="true"
+                password={formLogin.password}
+                type="password"
+                required
+              />
             </div>
 
-            <button onClick={handleClick} type='submit'>Login</button>
+            <button onClick={handleClick} type="submit">
+              {t("navbar.login")}
+            </button>
           </form>
 
-          <div className='login-bottom'>
-            {wrongMsg && <h3 style={{ color: 'red', textAlign: 'center' }}>{wrongMsg}</h3>}
+          <div className="login-bottom">
+            {wrongMsg && (
+              <h3 style={{ color: "red", textAlign: "center" }}>{wrongMsg}</h3>
+            )}
             {/* {<h4>Don't have an account yet? <a>Sign Up</a></h4>} */}
           </div>
         </>
-      }
+      )}
     </div>
-
-  )
+  );
 }
 
-export default LoginBox
+export default LoginBox;
--- client/src/Screens/Register_Page/register_box/RegisterBox.jsx
+++ client/src/Screens/Register_Page/register_box/RegisterBox.jsx
@@ -1,93 +1,130 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 
 // import { createNewUser, loginUser } from '../../API/ServerAPI/server.users';
-import { appContext } from '../../../context/appContext';
-import { useNavigate } from 'react-router-dom';
-import Spinner from '../../../components/Spinner/Spinner'
+import { appContext } from "../../../context/appContext";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../../components/Spinner/Spinner";
+import { useTranslation } from "react-i18next";
 
-import './RegisterBox.css'
-import { Link } from 'react-router-dom';
-import { createNewUser } from '../../../api/users.api';
+import "./RegisterBox.css";
+import { Link } from "react-router-dom";
+import { createNewUser } from "../../../api/users.api";
 
 const initialState = {
-  firstName: '',
-  lastName: '',
-  age: '',
-  email: '',
-  password: ''
-}
-
+  firstName: "",
+  lastName: "",
+  age: "",
+  email: "",
+  password: "",
+};
 
 function RegisterBox() {
   const [formRegister, setFormRegister] = useState(initialState);
-  const [errorMsg, setErrorMsg] = useState(false)
-  const [spinner, setSpinner] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [spinner, setSpinner] = useState(false);
   const navigate = useNavigate();
   // Global State
   const { setToken, setUserSignIn } = useContext(appContext);
 
   const handleChange = ({ target: { name: nameInput, value } }) => {
-    setFormRegister({ ...formRegister, [nameInput]: value })
-
-  }
-
+    setFormRegister({ ...formRegister, [nameInput]: value });
+  };
 
   const handleClick = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const { firstName, lastName, age, email, password } = formRegister;
-    if (!firstName || !lastName || !age || !email || !password) return setErrorMsg('Please enter all fields');
+    if (!firstName || !lastName || !age || !email || !password)
+      return setErrorMsg("Please enter all fields");
 
-    setSpinner(true)
+    setSpinner(true);
     const userCreated = await createNewUser({ ...formRegister, age: +age });
-    setSpinner(false)
-    if (userCreated.response && userCreated.response.status === 400) return setErrorMsg(userCreated.response.statusText);
+    setSpinner(false);
+    if (userCreated.response && userCreated.response.status === 400)
+      return setErrorMsg(userCreated.response.statusText);
 
     const userToken = userCreated.token;
     setToken(userToken);
     setUserSignIn(userCreated.user);
-    localStorage.setItem('token', userToken);
-    localStorage.setItem('user', userCreated.user);
-    navigate('/');
-  }
+    localStorage.setItem("token", userToken);
+    localStorage.setItem("user", userCreated.user);
+    navigate("/");
+  };
+  const { t } = useTranslation();
 
   return (
-    <div className='register-container'>
-
-      {spinner ? <Spinner /> :
+    <div className="register-container">
+      {spinner ? (
+        <Spinner />
+      ) : (
         <>
-          <form className='register-form' action="">
-            <div className='register-inputs'>
+          <form className="register-form" action="">
+            <div className="register-inputs">
+              <input
+                onChange={handleChange}
+                placeholder={t("form.firstname")}
+                name="firstName"
+                value={formRegister.firstName}
+                type="text"
+                required
+              />
 
-              <input onChange={handleChange} placeholder="First Name"
-                name='firstName' value={formRegister.firstName} type="text" required />
+              <input
+                onChange={handleChange}
+                placeholder={t("form.lastname")}
+                name="lastName"
+                value={formRegister.lastName}
+                type="text"
+                required
+              />
 
-              <input onChange={handleChange} placeholder="Last Name"
-                name='lastName' value={formRegister.lastName} type="text" required />
+              <input
+                onChange={handleChange}
+                placeholder={t("form.age")}
+                name="age"
+                value={formRegister.age}
+                type="number"
+                required
+              />
 
-              <input onChange={handleChange} placeholder="Age"
-                name='age' value={formRegister.age} type="number" required />
+              <input
+                onChange={handleChange}
+                placeholder={t("form.email")}
+                name="email"
+                value={formRegister.email}
+                type="email"
+                required
+              />
 
-              <input onChange={handleChange} placeholder="Email"
-                name='email' value={formRegister.email} type="email" required />
-
-              <input onChange={handleChange} placeholder="Password"
-                name='password' autoComplete="true" password={formRegister.password} type="password" required />
+              <input
+                onChange={handleChange}
+                placeholder={t("form.password")}
+                name="password"
+                autoComplete="true"
+                password={formRegister.password}
+                type="password"
+                required
+              />
             </div>
 
-
-
-            <button onClick={handleClick} type='submit'>Register</button>
+            <button onClick={handleClick} type="submit">
+              {t("form.Register")}
+            </button>
           </form>
 
-          <div className='register-bottom'>
-            {errorMsg && <h3 style={{ color: 'red', textAlign: 'center'  }}>{errorMsg}</h3>}
-            {<h4>Do you already have an account? <Link to='/login'>Log in</Link></h4>}
-
+          <div className="register-bottom">
+            {errorMsg && (
+              <h3 style={{ color: "red", textAlign: "center" }}>{errorMsg}</h3>
+            )}
+            {
+              <h4>
+                {t("form.massage")} <Link to="/login">{t("navbar.login")}</Link>
+              </h4>
+            }
           </div>
         </>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default RegisterBox;
